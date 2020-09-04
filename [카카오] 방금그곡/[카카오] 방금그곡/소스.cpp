659597d18f49ddcc7dbdce49a3e0316d028525af
yujin_Lee <sofod12@naver.com>
2020-09-04 20:28:07 +0900

    20.09.04

--- [카카오] 방금그곡/[카카오] 방금그곡/소스.cpp
+++ [카카오] 방금그곡/[카카오] 방금그곡/소스.cpp
@@ -6,6 +6,57 @@
 
 using namespace std;
 
+int note[12]; // 불일치시 이동할 간격
+int note2Int(string s) {
+
+	if (s == "C")
+		return 0;
+	else if (s == "C#")
+		return 1;
+	else if (s == "D")
+		return 2;
+	else if (s == "D#")
+		return 3;
+	else if (s == "E")
+		return 4;
+	else if (s == "F")
+		return 5;
+	else if (s == "F#")
+		return 6;
+	else if (s == "G")
+		return 7;
+	else if (s == "G#")
+		return 8;
+	else if (s == "A")
+		return 9;
+	else if (s == "A#")
+		return 10;
+	else if (s == "B")
+		return 11;
+
+	return -1;
+}
+void init(vector<string> m) {
+
+	for (int i = 0; i < 12; i++)
+		note[i] = -1;
+
+	//보이어무어 적용
+	int index = 0;
+	for (int i = m.size() - 1; i >= 0; i--) {
+		if (note[note2Int(m[i])] == -1) {
+			note[note2Int(m[i])] = index;
+			index++;
+		}
+	}
+	for (int i = 0; i < 12; i++) {
+		if (note[i] == -1)
+			note[i] = m.size();
+	}
+
+}
+
+
 
 vector<string> toMelody(string m) {
 	queue<int> sharp;
@@ -85,7 +136,7 @@ bool solve(vector<string> melody, vector<string> sheet) { //문자열 비교
 	int ssize = sheet.size();
 	int gap;
 
-	bool isOk = false;
+	bool isOk;
 	for (int i = 0; i < ssize - msize + 1; i++) {
 		isOk = true;
 		for (int j = 0; j < msize; j++) {
@@ -99,6 +150,46 @@ bool solve(vector<string> melody, vector<string> sheet) { //문자열 비교
 
 	return false;
 }
+
+void printV(vector<string> v) {
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+
+	cout << endl;
+}
+bool solve2(vector<string> melody, vector<string> sheet) {
+	
+	int msize = melody.size();
+	int ssize = sheet.size();
+	int gap;
+	bool check = false;
+	for (int i = msize-1 ; i < ssize ; i++) {
+
+	   //끝자리 확인
+	   if (sheet[i] != melody[msize - 1]) {
+	       i += note[note2Int(sheet[i])]; //끝이 다르니까 건너뛰기
+	       i--;
+	       if (i > ssize)
+	           return false;
+	       continue;
+	   }
+	   else {
+		   check = true;
+		   //끝자리가 같음 --> 비교시작
+		   for (int index = 0; index < msize; index++) {
+			   if (sheet[i - index] != melody[msize - 1 - index]) { //비교 실패
+				   check = false;
+				   break;
+			   }
+		   }
+		   if (check)
+			   return true; //비교 성공! 일치
+	   }
+	}
+
+	return false;
+}
+
 bool cmp(pair< int, int > a, pair< int, int > b) {
 
 	if (a.second == b.second)
@@ -106,8 +197,6 @@ bool cmp(pair< int, int > a, pair< int, int > b) {
 	else
 		return b.second < a.second;
 }
-
-
 
 string solution(string m, vector<string> musicinfos) {
 	string answer = "";
@@ -118,7 +207,20 @@ string solution(string m, vector<string> musicinfos) {
 
 
 	vector< pair< int, int > > right;
-	// 문자열 탐색
+
+	/*
+	보이어 무어 탐색버전 --> 문자열이 적어서 효율적이지못하다
+		
+		init(melody);
+		for (int i = 0; i < info.size(); i++) {
+			vector<string> sheet = toSheet(info[i]);
+			if (solve2(melody, sheet))
+				right.push_back({ i , toMin(info[i][1]) - toMin(info[i][0]) });
+		}
+
+	
+	*/
+
 	for (int i = 0; i < info.size(); i++) {
 		vector<string> sheet = toSheet(info[i]);
 		if (solve(melody, sheet))  // 정답임 
@@ -129,7 +231,7 @@ string solution(string m, vector<string> musicinfos) {
 	if (right.empty())
 		answer = "(None)";
 	else {
-		//중복조건
+		//중복조건에 맞춰서 정렬후 출력
 		sort(right.begin(), right.end(), cmp);
 		answer = info[right.front().first][2];
 	}
@@ -139,8 +241,8 @@ string solution(string m, vector<string> musicinfos) {
 
 int main() {
 
-	string m = "ABC#";
-	vector<string> musicinfos = { "03:00,03:40,ONE,C#DAB", "04:00,05:40,TWO,FFFABC#FFF" };
+	string m = "ABC";
+	vector<string> musicinfos = { "12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF" };
 
 	cout << solution(m, musicinfos);
 
