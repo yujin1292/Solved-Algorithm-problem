b3aa74c7af49e59c858531daee8e40d060099f11
Yujin Lee <sofod12@naver.com>
2022-02-02 21:20:43 +0900

    2022-02-02

---
+++ BOJ/[백준] 2011 암호코드/[백준] 2011 암호코드/소스.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#define ll long long
+using namespace std;
+
+const int MOD = 1000000;
+string code = "";
+ll dp[5001];
+
+
+ll decode(int start) {
+	// 메모이제이션
+	if (dp[start] != -1) return dp[start];
+
+	int len = code.size() - start;
+
+	if (len <= 0) return 0;
+	else if (code[start] == '0') return 0;
+	else if (len == 1) return 1;
+
+	int top = (code[start] - '0') * 10 + (code[start + 1] - '0');
+
+	if (top <= 26) {
+		dp[start] = (decode(start + 1) % MOD + decode(start + 2) % MOD) % MOD;
+		if (len == 2) dp[start] = (dp[start] + 1) % MOD;
+		return dp[start];
+	}
+	else {
+		dp[start] = decode(start + 1) % MOD;
+		return dp[start];
+	}
+}
+
+int main() {
+
+	cin >> code;
+
+	fill(dp, dp + code.size(), -1);
+	
+	cout << decode(0);
+	
+	return 0;
+}
